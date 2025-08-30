a0c5b72f9ab5d88dd5afb18fd3dca6c1223c03ac
Ricardo <ricardohidalgo2004@gmail.com>
2025-08-30 02:47:35 -0500

    add linter

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,13 @@
-export default function RootLayout({
+import '@/app/ui/global.css'
+
+export default function RootLayout ({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body>{children}</body>
     </html>
-  );
+  )
 }
