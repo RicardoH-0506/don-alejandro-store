53cd2a6eddf76da1f87892dda186fdf9dda42024
Ricardo <ricardohidalgo2004@gmail.com>
2025-09-03 16:54:55 -0500

    delete

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -16,7 +16,6 @@ export default function Search ({ placeholder }: { placeholder: string }) {
     } else {
       params.delete('query')
     }
-    params.set('page', '1')
 
     replace(`${pathname}?${params.toString()}`)
   }, 300)
